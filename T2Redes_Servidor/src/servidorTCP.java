83dbc142463c575dcc5f5dfd64cb74b4b6781682
Svartehjerte <guillermo.fernanb.12@alumnos.usm.cl>
2014-06-01 05:56:02 -0400

    Guarda en archivo
    Agregué código que lo que hace es guardar en un archivo lo que le llega
    al servidor TCP.

--- T2Redes_Servidor/src/servidorTCP.java
+++ T2Redes_Servidor/src/servidorTCP.java
@@ -11,8 +11,28 @@ public class servidorTCP {
 		while(true){
 			Socket socketConexion = servidorTCPSocket.accept();
 			BufferedReader desdeCliente = new BufferedReader(new InputStreamReader(socketConexion.getInputStream()));
+			
 			recibidoDesdeCliente = desdeCliente.readLine();
 			System.out.println("Recibido: " + recibidoDesdeCliente);
+			
+			try{
+				// Se crea el .txt con la lista de contactos
+				File datosRecibidos = new File("datos_recibidos.txt");
+				
+				// El (..., true) es para que se agregue lo leido al final del archivo -append-)
+				BufferedWriter escritor = new BufferedWriter(new FileWriter(datosRecibidos, true));
+				
+				// Escribo en el archivo nuevo dado el siguiente formato
+				escritor.write(recibidoDesdeCliente);
+				escritor.newLine();
+				
+				// Cierro escritor
+				escritor.close();
+			}
+			catch(Exception ioe){
+				System.out.println(ioe.toString());
+			}
+			
 			if (flag){
 				servidorTCPSocket.close();
 				break;

0a911220637bd558b98892b8fdfd9535473ef3a7
Álvaro <alvaro.rojasv@alumnos.usm.cl>
2014-06-01 01:33:52 -0400

    arreglo menor while
    deje listo para cuando se mande el msj para modificar la flag desde
    cliente y parar el servidor tcp, para que no queden tomado el socket

--- T2Redes_Servidor/src/servidorTCP.java
+++ T2Redes_Servidor/src/servidorTCP.java
@@ -5,15 +5,19 @@ import java.util.*;
 public class servidorTCP {
 	public static void main(String argv[]) throws Exception{
 		int puerto = 7777;
+		boolean flag = false;
 		String recibidoDesdeCliente;
 		ServerSocket servidorTCPSocket = new ServerSocket(puerto);
-		
 		while(true){
 			Socket socketConexion = servidorTCPSocket.accept();
 			BufferedReader desdeCliente = new BufferedReader(new InputStreamReader(socketConexion.getInputStream()));
-			
 			recibidoDesdeCliente = desdeCliente.readLine();
 			System.out.println("Recibido: " + recibidoDesdeCliente);
-		}
+			if (flag){
+				servidorTCPSocket.close();
+				break;
+			}
+				//En alguna parte hay que leer lo que manda cliente para parar este while y cerrar el socket
+		}	
 	}
 }
